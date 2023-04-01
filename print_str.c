26d99561c649639553aa6aa8bfbacd7e9386c024
Bassey Ukeme <basseyukemme@gmail.com>
2023-04-01 16:45:14 +0100

    added task 1

--- print_str.c
+++ print_str.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * print_s -> prints string
+ * @s: string
+ * @count: number of characters
+ */
+
+void print_s(char *s, int *count)
+{
+	while (*s)
+	{
+		_putchar(*s);
+		s++;
+		(*count)++;
+	}
+}
+
+/**
  * print_str -> print string
  * @args: arguments passed
  * Return: count
@@ -11,12 +27,12 @@ int print_str(va_list *args)
 	char *s = va_arg(*args, char *);
 	int count = 0;
 
-	while (*s)
+	if (!s)
 	{
-		_putchar(*s);
-		s++;
-		count++;
+		print_s("(null)", &count);
+		return (count);
 	}
+	print_s(s, &count);
 	return (count);
 }
 
