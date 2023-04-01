6593e4c3d0144dc7bc3a36fb440f8d0dac287184
Bassey Ukeme <basseyukemme@gmail.com>
2023-04-01 21:51:23 +0100

    edited task 1

--- print_int.c
+++ print_int.c
@@ -3,15 +3,20 @@
 /**
  * _print_int -> prints number and calcuate count
  * @n: number
+ * @count: number of digits
  */
 
-void _print_int(int n)
+void _print_int(int n, int *count)
 {
 	if (n / 10 != 0)
 		_print_int(n / 10);
 	if (n / 10 == 0 && n < 0)
+	{
 		_putchar('-');
+		(*count)++;
+	}
 	_putchar(((n < 0) ? n * -1 : n) % 10 + '0');
+	(*count)++;
 }
 
 /**
@@ -25,11 +30,6 @@ int print_int(va_list *args)
 	int num = va_arg(*args, int);
 	int count = 0;
 
-	_print_int(num);
-	while (num % 10 || num)
-	{
-		num = num / 10;
-		count++;
-	}
+	_print_int(num, &count);
 	return (count);
 }
