0775c9cdbc1b9611a373844ad559cecbf35607dd
Bassey Ukeme <basseyukemme@gmail.com>
2023-04-03 16:23:49 +0100

    task 5

--- print_s.c
+++ print_s.c
@@ -19,8 +19,6 @@ int print_s_cap(va_list *args)
 		{
 			_putchar('\\');
 			_putchar('x');
-			if (temp > 0 && temp <= 11)
-				_putchar(0 + '0');
 			_printf("%x", temp);
 			count = count + 2;
 		}
